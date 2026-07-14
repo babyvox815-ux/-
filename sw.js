d9095d0122af748db4aaa13c6d48e7df4d1df866
babyvox815-ux <babyvox815@gmail.com>
2026-07-15 00:34:06 +0900

    Refresh cache for cropped workout imagery

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='용호운동-v24';
-const ASSETS=['./','./index.html','./styles.css?v=21','./app.js?v=24','./manifest.webmanifest'];
+const CACHE='용호운동-v25';
+const ASSETS=['./','./index.html','./styles.css?v=21','./image-fix.css?v=25','./assets.js?v=25','./app.js?v=25','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});

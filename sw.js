f9d85b25686ac427e00d67d9156912b982be41ac
babyvox815-ux <babyvox815@gmail.com>
2026-07-15 01:52:58 +0900

    Activate v27 workout image assets and clear old cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='용호운동-v25';
-const ASSETS=['./','./index.html','./styles.css?v=21','./image-fix.css?v=25','./assets.js?v=25','./app.js?v=25','./manifest.webmanifest'];
+const CACHE='용호운동-v27';
+const ASSETS=['./','./index.html','./styles.css?v=27','./image-fix.css?v=27','./assets-v27.js?v=27','./app.js?v=27','./manifest.webmanifest?v=27'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});

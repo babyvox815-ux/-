9ce581e1e44884aad85aca07ebd490e7f2dfcd68
babyvox815-ux <babyvox815@gmail.com>
2026-07-14 20:44:12 +0900

    Refresh service worker cache for date-first app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ygo-v6';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./data.js','./core1.js','./core2.js','./manifest.webmanifest'];
+const CACHE='ygo-v8';
+const ASSETS=['./','./index.html','./styles.css?v=8','./app.js?v=8','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))) });
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});

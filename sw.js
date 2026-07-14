edbeab9e29257f7119e69d2f9349396ca87434fd
babyvox815-ux <babyvox815@gmail.com>
2026-07-15 01:30:39 +0900

    Refresh cache for v26 cropped workout images

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='용호운동-v26';
+const ASSETS=['./','./index.html','./styles.css?v=21','./image-fix.css?v=25','./image-layout-v26.css?v=26','./assets.js?v=25','./app.js?v=26','./manifest.webmanifest'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))});
